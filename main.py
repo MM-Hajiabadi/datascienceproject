8fb006981b0e63d675e8e2e88821880b4271ea9e
MM-Hajiabadi <m.hja_76@outlook.com>
2026-02-02 19:31:43 +0330

    Model Evaluation is Updated

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from src.datascience.pipeline.data_transformation_pipeline import (
 from src.datascience.pipeline.data_validation_pipeline import (
     DataValidationTrainingPipeline,
 )
+from src.datascience.pipeline.model_evaluation_pipeline import (
+    ModelEvaluationTrainingPipeline,
+)
 from src.datascience.pipeline.model_trainer_pipeline import (
     ModelTrainerTrainingPipeline,
 )
@@ -47,6 +50,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.initiate_model_training()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelEvaluationTrainingPipeline()
+    model_trainer.initiate_model_evaluation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
